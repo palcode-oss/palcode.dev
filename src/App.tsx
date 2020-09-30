b61e0d8e922b4181c328974a6fe9cc3dfa97f674
Pal Kerecsenyi <palkerecs@gmail.com>
2020-09-30 17:03:38 +0100

    Setup TS and initialize firebase

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDvtCpVfSv_WyqODPmPlrYvGqCmlZZlbk8",
+  authDomain: "palcode-ba70e.firebaseapp.com",
+  databaseURL: "https://palcode-ba70e.firebaseio.com",
+  projectId: "palcode-ba70e",
+  storageBucket: "palcode-ba70e.appspot.com",
+  messagingSenderId: "1066769258920",
+  appId: "1:1066769258920:web:472b48d2d97f99f99fa59d",
+});
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
