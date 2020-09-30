70d0dc4273bc0cfc96343518297ecbaf3c6d5409
Geza Kerecsenyi <gezakerecsenyi@gmail.com>
2020-09-30 18:12:45 +0100

    Add (unfunctional) navbar

---
+++ src/Settings.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export default function Settings(): ReactElement {
+    return (
+        <div className='settings'>
+
+        </div>
+    )
+}
--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,12 +1,71 @@
 import React, { ReactElement } from "react";
 import { useAuth } from "../helpers/auth";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { faCog } from "@fortawesome/free-solid-svg-icons/faCog";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons/faSignOutAlt";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons/faSignInAlt";
+import { Shimmer } from "react-shimmer";
 
 export default function Navbar(): ReactElement {
     const [authUser, authLoading] = useAuth();
 
     return (
         <div className='navbar'>
+            <h1 className='title'>
+                <Link to='/'>
+                    PalCode
+                </Link>
+            </h1>
 
+            <div className='nav-options'>
+                {
+                    authUser && (
+                        <>
+                            <Link
+                                className='option'
+                                to='/'
+                            >
+                                <FontAwesomeIcon icon={faHome}/>
+                                &nbsp;Dashboard
+                            </Link>
+                            <Link
+                                className='option'
+                                to='/user/settings'
+                            >
+                                <FontAwesomeIcon icon={faCog}/>
+                                &nbsp;Settings
+                            </Link>
+                            <button className='option sign-out'>
+                                <FontAwesomeIcon icon={faSignOutAlt}/>
+                                &nbsp;Sign out
+                            </button>
+                        </>
+                    )
+                }
+
+                {
+                    !authLoading && !authUser && (
+                        <button className='option'>
+                            <FontAwesomeIcon icon={faSignInAlt}/>
+                            &nbsp;Sign in
+                        </button>
+                    )
+                }
+
+                {
+                    authLoading && (
+                        <button className='option loading'>
+                            <Shimmer
+                                height={12}
+                                width={100}
+                                className='shimmer'
+                            />
+                        </button>
+                    )
+                }
+            </div>
         </div>
     )
 }
