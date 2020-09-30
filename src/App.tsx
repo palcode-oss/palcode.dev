21024822852d8612ed833ee300bc98e820537f30
Geza Kerecsenyi <gezakerecsenyi@gmail.com>
2020-09-30 17:43:21 +0100

    Reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,39 @@ import firebase from 'firebase';
 import { useAuth } from './helpers/auth';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDvtCpVfSv_WyqODPmPlrYvGqCmlZZlbk8",
-  authDomain: "palcode-ba70e.firebaseapp.com",
-  databaseURL: "https://palcode-ba70e.firebaseio.com",
-  projectId: "palcode-ba70e",
-  storageBucket: "palcode-ba70e.appspot.com",
-  messagingSenderId: "1066769258920",
-  appId: "1:1066769258920:web:472b48d2d97f99f99fa59d",
+    apiKey: "AIzaSyDvtCpVfSv_WyqODPmPlrYvGqCmlZZlbk8",
+    authDomain: "palcode-ba70e.firebaseapp.com",
+    databaseURL: "https://palcode-ba70e.firebaseio.com",
+    projectId: "palcode-ba70e",
+    storageBucket: "palcode-ba70e.appspot.com",
+    messagingSenderId: "1066769258920",
+    appId: "1:1066769258920:web:472b48d2d97f99f99fa59d",
 });
 
 function App() {
-  const [authUser, authLoading] = useAuth();
+    const [authUser, authLoading] = useAuth();
 
-  return (
-    <div>
-      {authLoading && (
-          <p>
-            loading...
-          </p>
-      )}
+    return (
+        <div>
+            {authLoading && (
+                <p>
+                    loading...
+                </p>
+            )}
 
-      {!authUser && !authLoading && (
-          <p>
-            sign in
-          </p>
-      )}
+            {!authUser && !authLoading && (
+                <p>
+                    sign in
+                </p>
+            )}
 
-      {authUser && (
-          <p>
-            signed in
-          </p>
-      )}
-    </div>
-  );
+            {authUser && (
+                <p>
+                    signed in
+                </p>
+            )}
+        </div>
+    );
 }
 
 export default App;
