1c49a729045bd88c33a26dba59f89a1aa2d8ad9c
Pal Kerecsenyi <palkerecs@gmail.com>
2020-10-02 19:02:53 +0100

    Linting bootstrap

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const apiGet = require("./get");
 const apiSave = require("./save");
+const apiCode = require("./code");
 
 router.use(apiGet);
 router.use(apiSave);
+router.use(apiCode);
 
 module.exports = router;
