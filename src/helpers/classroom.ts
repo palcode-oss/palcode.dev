fe6ceb85257dd9949b365d672992ef2c848a226f
Geza Kerecsenyi <gezakerecsenyi@gmail.com>
2020-10-03 19:13:23 +0100

    Style loader spinner

--- src/helpers/classroom.ts
+++ src/helpers/classroom.ts
@@ -38,3 +38,32 @@ export function useClassrooms(userId: string): [Classroom[], boolean] {
 
     return [classrooms, classroomsLoading];
 }
+
+export function useOwnedClassroom(userId: string, updater?: any): [Classroom[], boolean] {
+    const [classroomData, setClassroomData] = useState<Classroom[]>([]);
+    const [classroomDataLoading, setClassroomDataLoading] = useState(true);
+
+    useEffect(() => {
+        setClassroomDataLoading(true);
+        async function loadClassroomData() {
+            const data = await firebase
+                .firestore()
+                .collection('classrooms')
+                .where('owner', '==', userId)
+                .get()
+                .then(data => data.docs);
+
+            setClassroomData(
+                data.map((doc) => ({
+                    ...doc.data() as Classroom,
+                    id: doc.id,
+                })),
+            );
+            setClassroomDataLoading(false);
+        }
+
+        loadClassroomData();
+    }, [updater, userId]);
+
+    return [classroomData, classroomDataLoading];
+}
